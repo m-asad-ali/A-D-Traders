1ba4fd584f0c80e2a378ade1af6f1b8dc480acdd
m-asad-ali <masadali304@gmail.com>
2023-04-16 22:43:16 +0500

    Data has been added named as testing.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,21 @@
 const products = [
   {
+    id: 1,
+    name: "iphone x",
+    company: "apple",
+    price: 6000000,
+    colors: ["#ff0000", "#000000", "#CDD0D0"],
+    image:
+      "https://images.pexels.com/photos/1275229/pexels-photo-1275229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description:
+      "The mobile is compact with its 6.2-inch OLED screen and far lighter at 168g. It perfectly captures the design, looks, and feel of the expensive one. It comes with a snapdragon processor with a 5n chip in it. It has a 200mp camera in the rear 100mp in front perfect for selfie lovers. It also support HDR content means you can watch 4K content on it.",
+    category: "mobile",
+    featured: true,
+    stock: 10,
+    stars: 4.5,
+    reviews: 58,
+  },
+  {
     id: "thapaserialnoa",
     name: "iphone x",
     company: "apple",
